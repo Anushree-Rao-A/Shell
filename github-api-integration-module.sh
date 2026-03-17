51d84b5cccd98213d8d47b68d5c848ddc33b9736
unknown <anushreeraoaravinda@gmail.com>
2026-03-17 23:16:49 +0530

    update github.sh second change

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -41,6 +41,7 @@ else
 
     # yes - this result is on multiple pages
     #yessssssssssssssssssssssss
+    #noooooo
     for p in `seq 1 $last_page`; do
         rest_call "https://api.github.com${GITHUB_API_REST}?page=$p"
     done
