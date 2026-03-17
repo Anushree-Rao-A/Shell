94b21336a204d4c9ada2eef9825aeae718b45b5e
unknown <anushreeraoaravinda@gmail.com>
2026-03-17 23:36:30 +0530

    update github.sh 4 change

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -27,7 +27,7 @@ TMPFILE=`mktemp /tmp/${temp}.XXXXXX` || exit 1
 
 
 function rest_call {
-    curl -s $1 -H "${GITHUB_API_HEADER_ACCEPT}" -H "Authorization: token $GITHUB_TOKEN" >> $TMPFILE
+    curl -s $1 -H "${GITHUB_API_HEADER_ACCEPT}" -H "Authorization: token $GITHUB_TOKEN" >> $TMPFILEEE
 }
 
 # single page result-s (no pagination), have no Link: section, the grep result is empty
